d2b017992c6730246b3fc97772d058846e4e31af
José Ramírez <jose.ramirez.a@est.una.ac.cr>
2024-04-27 10:16:45 -0600

    VerifyFiltersMenuDisplayed

---
+++ src/main/java/patterns/pageobject/FiltersPage.java
@@ -0,0 +1,62 @@
+package patterns.pageobject;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class FiltersPage extends PageHeader {
+
+    @FindBy(css="aside#column-left")
+    private WebElement filtersMenu;
+
+    @FindBy(css="aside#column-left a.list-group-item")
+    private List<WebElement> filtersCategories;
+
+    private WebDriver driver;
+
+    // Constructor
+    public FiltersPage(WebDriver driver) {
+
+        super(driver);
+        this.driver = driver;
+
+    }
+
+    public void clickDesktopsCategory(){
+
+        WebDriverWait explicitWait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        explicitWait.until(ExpectedConditions.visibilityOf(getDesktopsCategory()));
+        new Actions (driver)
+            .moveToElement(getDesktopsCategory())
+            .click(getDesktopsCategory())
+            .perform();
+
+    }
+
+    public void clickShowAllDesktops(){
+
+        WebDriverWait explicitWait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        explicitWait.until(ExpectedConditions.visibilityOf(getShowAllDesktops()));
+        new Actions (driver)
+            .moveToElement(getShowAllDesktops())
+            .click(getShowAllDesktops())
+            .perform();
+
+    }
+
+    public WebElement getFiltersMenu(){
+
+        WebDriverWait explicitWait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        explicitWait.until(ExpectedConditions.visibilityOf(filtersMenu));
+        return filtersMenu;
+    }
+
+    public List<WebElement> getFiltersCategories(){return filtersCategories;}
+
+}
