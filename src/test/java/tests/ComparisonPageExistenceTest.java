cae800bc1b628b0e09f7ce7e6bca555515d34e99
SantiagoRocha <xxsantirocha12xx@gmail.com>
2024-04-29 09:33:30 -0500

    Merge from remote branch done

--- src/test/java/tests/ComparisonPageExistenceTest.java
+++ src/test/java/tests/ComparisonPageExistenceTest.java
@@ -1,30 +1,48 @@
 package tests;
 
-import org.openqa.selenium.Cookie;
-import org.openqa.selenium.WebDriver;
-import org.sikuli.script.FindFailed;
-import org.sikuli.script.Pattern;
-import org.sikuli.script.Screen;
-import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
 import patterns.DriverManager;
-import patterns.pageobject.CamerasPage;
-import patterns.pageobject.ComparisonPage;
+import patterns.DriverManager.BrowserType;
 import utilities.Utils;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class VerifyComparisonToolAppropriateAndDisplaysFeatures {
-    private WebDriver driver = null;
+import org.openqa.selenium.Cookie;
+import org.sikuli.script.FindFailed;
+import org.sikuli.script.Pattern;
+import org.sikuli.script.Screen;
+import patterns.pageobject.*;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.*;
+import org.testng.Assert;
+
+public class ComparisonPageExistenceTest {
+
+    private WebDriver driver;
+
     Screen screen = new Screen();
     String pathYourSystem = System.getProperty("user.dir") + "\\";
     Pattern image = new Pattern(pathYourSystem+"src\\resources\\cloudflare.png");
 
     @BeforeTest
-    public void beforeTest() throws FindFailed{
+    @Parameters("browserType")
+    public void beforeTest(String browserType) {
+        driver = DriverManager.getDriver(BrowserType.valueOf(browserType));
+        Date today = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("yyyyMMddHHmmss");
+        String formatedDate = format.format(today);
+        driver.get("https://demo.opencart.com/");
+        driver.manage().addCookie(new Cookie("OCSESSID","11c0f931cf"+formatedDate+"ec"));
+        driver.manage().addCookie(new Cookie("_ga","GA1.1.2123778129.1713796835"));
+        driver.manage().addCookie(new Cookie("_ga_X8G0BRFSDF","GS1.1.1713796835.1.0.1713796835.0.0.0"));
+        driver.manage().addCookie(new Cookie("_gcl_au","1.1.534898992.1713796834"));
+        driver.manage().addCookie(new Cookie("_gid","GA1.2.438931849.1713796835"));
+        driver.manage().addCookie(new Cookie("cf_clearance","zJ9wxfXGd6JiMI3czkXFs4.kzRi6IqvPGPR1BaphLjM-1713852454-1.0.1.1-XKiVE5CVgEaZJ6pwxaPFZvAbzObkzBLWVzgfCCZoPHgWbHPgp6V.HROlod2Rr0jRzg2O5vNoDLVqbRP0JC8Gnw"));
+        driver.manage().addCookie(new Cookie("currency","USD"));
+    }
+
+    @Test(priority = 1, description = "Verify cart page elements")
+    public void cartPageElementsTest() {
         driver = DriverManager.getDriver(DriverManager.BrowserType.EDGE); // replace with your desired browser
         //Login as admin to unlock functionalities
         Date today = new Date();
@@ -77,8 +95,7 @@ public class VerifyComparisonToolAppropriateAndDisplaysFeatures {
     }
 
     @AfterTest
-    public void afterTest(){
-        //driver.quit();
+    public void afterTest() {
         DriverManager.quitDriver();
     }
 }
